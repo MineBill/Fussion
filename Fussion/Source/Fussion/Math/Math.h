ec27003ca6e8185b28d5c4e23e3ffdb7e10d953b
MineBill <minebill@protonmail.com>
2024-07-05 19:41:04 +0300

    Some math functions.

---
+++ Fussion/Source/Fussion/Math/Math.h
@@ -0,0 +1,27 @@
+﻿#pragma once
+#include "Fussion/Core/Types.h"
+
+namespace Fussion::Math {
+    auto Min(ScalarType auto first, ScalarType auto second) -> decltype(first)
+    {
+        if (first < second)
+            return first;
+        return second;
+    }
+
+    auto Max(ScalarType auto first, ScalarType auto second) -> decltype(first)
+    {
+        if (first > second)
+            return first;
+        return second;
+    }
+
+    auto Clamp(ScalarType auto value, ScalarType auto min, ScalarType auto max) -> decltype(value)
+    {
+        if (value < min)
+            value = min;
+        if (value > max)
+            value = max;
+        return value;
+    }
+}
