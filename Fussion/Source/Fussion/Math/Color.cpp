eb9662ea096f70f149045926acea40925a81770a
MineBill <minebill@protonmail.com>
2024-07-06 12:36:03 +0300

    Color stuff.

---
+++ Fussion/Source/Fussion/Math/Color.cpp
@@ -0,0 +1,30 @@
+﻿#include "e5pch.h"
+#include "Color.h"
+
+namespace Fussion {
+Color Color::White{ 1, 1, 1, 1 };
+Color Color::Red{ 1, 0, 0, 1 };
+Color Color::Green{ 0, 1, 0, 1 };
+Color Color::Blue{ 0, 0, 1, 1 };
+Color Color::Yellow{ 1, 1, 0, 1 };
+Color Color::Magenta{ 1, 0, 1, 1 };
+Color Color::Cyan{ 0, 1, 1, 1 };
+Color Color::Black{ 0, 0, 0, 1 };
+Color Color::Purple{ 0.5, 0, 0.5, 1 };
+Color Color::Orange{ 1, 0.5, 0, 1 };
+Color Color::Pink{ 1, 0.75, 0.8, 1 };
+Color Color::Turquoise{ 0, 0.8, 0.8, 1 };
+Color Color::Lime{ 0.75, 1, 0, 1 };
+Color Color::Gray{ 0.5, 0.5, 0.5, 1 };
+Color Color::Brown{ 0.6, 0.4, 0.2, 1 };
+Color Color::Maroon{ 0.5, 0, 0, 1 };
+Color Color::Teal{ 0, 0.5, 0.5, 1 };
+Color Color::Olive{ 0.5, 0.5, 0, 1 };
+Color Color::Navy{ 0, 0, 0.5, 1 };
+Color Color::Coral{ 1, 0.5, 0.3, 1 };
+Color Color::Rose{ 1, 0.2, 0.5, 1 };
+Color Color::SkyBlue{ 0.5, 0.75, 1, 1 };
+Color Color::ForestGreen{ 0.2, 0.5, 0.2, 1 };
+Color Color::DarkGoldenRod{ 0.72, 0.52, 0.04, 1 };
+Color Color::Indigo{ 0.29, 0, 0.51, 1 };
+}
