99bd84891ebfa1a0657a3e157ce68c36c0888fca
MineBill <minebill@protonmail.com>
2024-07-30 21:24:08 +0300

    Add Catch2.

--- Vendor.lua
+++ Vendor.lua
@@ -45,6 +45,18 @@ package("tinygltf")
 package_end()
 add_requires("tinygltf")
 
+package("Catch2")
+    add_deps("cmake")
+    set_sourcedir(path.join(os.scriptdir(), "Vendor/Catch2-3.6.0"))
+    on_install(function (package)
+        local configs = {}
+        table.insert(configs, "-DCMAKE_BUILD_TYPE=" .. (package:debug() and "Debug" or "Release"))
+        table.insert(configs, "-DBUILD_SHARED_LIBS=" .. (package:config("shared") and "ON" or "OFF"))
+        import("package.tools.cmake").install(package, configs)
+    end)
+package_end()
+add_requires("Catch2")
+
 target("glm", function()
     set_kind "headeronly"
     set_group "Vendor"
