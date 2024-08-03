97790e0e559c3ace5a0292e09b21d15a31c18e8e
MineBill <minebill@protonmail.com>
2024-08-03 12:11:43 +0300

    Display stacktrace when an assertion is hit.

--- Fussion/Source/Fussion/Core/Core.h
+++ Fussion/Source/Fussion/Core/Core.h
@@ -1,6 +1,7 @@
 #pragma once
 #include "Fussion/Log/Log.h"
 #include <source_location>
+#include <stacktrace>
 
 #if defined(_MSC_VER)
 #define BUILTIN_TRAP_FUNCTION __debugbreak
@@ -10,17 +11,22 @@
 
 #define USE_ASSERTIONS
 #ifdef USE_ASSERTIONS
-#define VERIFY(expr, ...)                                                                            \
-        {                                                                                            \
-            if (!(expr)) {                                                                           \
-                LOG_ERRORF("ASSERTION HIT: {}", #expr);                                              \
-                if constexpr (std::tuple_size<decltype(std::make_tuple(__VA_ARGS__))>::value != 0) { \
-                    LOG_ERRORF("    " __VA_ARGS__);                                                  \
-                } else {                                                                             \
-                    LOG_ERRORF("   No additional information provided");                             \
-                }                                                                                    \
-                BUILTIN_TRAP_FUNCTION();                                                             \
-            }                                                                                        \
+#define VERIFY(expr, ...)                                                                               \
+        {                                                                                               \
+            if (!(expr)) {                                                                              \
+                LOG_ERRORF("ASSERTION HIT: {}", #expr);                                                 \
+                if constexpr (std::tuple_size<decltype(std::make_tuple(__VA_ARGS__))>::value != 0) {    \
+                    LOG_ERRORF("    " __VA_ARGS__);                                                     \
+                } else {                                                                                \
+                    LOG_ERRORF("    No additional information provided");                               \
+            }                                                                                           \
+            u32 i = 0;                                                                                  \
+            LOG_ERRORF("Stacktrace:");                                                                  \
+            for (auto const& entry : std::stacktrace::current()) {                                      \
+                    LOG_ERRORF("{:^4} > {}:{}", i++, entry.source_file(), entry.source_line());         \
+                }                                                                                       \
+                BUILTIN_TRAP_FUNCTION();                                                                \
+            }                                                                                           \
         }
 #else
     #define EASSERT(expr, ...)
@@ -49,20 +55,20 @@
 #define UNREACHABLE PANIC("Reached unreachable code!")
 
 namespace Fussion {
-template<typename F>
-struct Defer {
-    explicit Defer(F f) : m_F(f) {}
-    ~Defer() { m_F(); }
+    template<typename F>
+    struct Defer {
+        explicit Defer(F f) : m_F(f) {}
+        ~Defer() { m_F(); }
 
-private:
-    F m_F;
-};
+    private:
+        F m_F;
+    };
 
-template<typename F>
-Defer<F> MakeDefer(F f)
-{
-    return Defer<F>(f);
-}
+    template<typename F>
+    Defer<F> MakeDefer(F f)
+    {
+        return Defer<F>(f);
+    }
 }
 
 #define DEFER_1(x, y) x##y
