0ab59008394ef7a5708b54235c4ac6d391405454
MineBill <minebill@protonmail.com>
2024-08-04 16:04:58 +0300

    Rect type.

---
+++ Fussion/Source/Fussion/Math/Rect.cpp
@@ -0,0 +1,28 @@
+﻿#include "W:\source\projects\Fussion\build\.gens\Fussion\windows\x64\debug\Fussion\Source\e5pch.h"
+#include "Rect.h"
+
+namespace Fussion {
+    Rect::Rect(f32 x, f32 y, f32 width, f32 height)
+        : Position(x, y), Size(width, height) {}
+
+    Rect::Rect(Vector2 const& position, Vector2 const& size)
+        : Position(position), Size(size) {}
+
+    Rect Rect::FromSize(Vector2 const& size)
+    {
+        return { Vector2::Zero, size };
+    }
+
+    Rect Rect::FromStartEnd(Vector2 const& start, Vector2 const& end)
+    {
+        return { start, end - start };
+    }
+
+    auto Rect::Contains(Vector2 const& point) const -> bool
+    {
+        auto end = Position + Size;
+        auto top_left = point.X >= Position.X && point.Y >= Position.Y;
+        auto bot_right = point.X < end.X && point.Y < end.Y;
+        return top_left && bot_right;
+    }
+}
---
+++ Fussion/Source/Fussion/Math/Rect.h
@@ -0,0 +1,28 @@
+﻿#pragma once
+#include "Vector2.h"
+
+#include <Fussion/Core/Types.h>
+#include <Fussion/Log/Formatters.h>
+
+namespace Fussion {
+    struct Rect {
+        Vector2 Position{};
+        Vector2 Size{};
+
+        Rect(f32 x, f32 y, f32 width, f32 height);
+        Rect(Vector2 const& position, Vector2 const& size);
+
+        /// Create a Rect only from size. The position is set to @ref Vector2 "test".
+        /// @return The rect.
+        static Rect FromSize(Vector2 const& size);
+
+        /// Create a rect from two positions. The size is calculated automatically.
+        /// @return The rect.
+        static Rect FromStartEnd(Vector2 const& start, Vector2 const& end);
+
+        /// Checks whether @p point is contained within this rect.
+        auto Contains(Vector2 const& point) const -> bool;
+    };
+}
+
+FSN_MAKE_FORMATTABLE(Fussion::Rect, "Rect({}, {})", v.Position, v.Size)
