bd65ef6bd0c6c4c40f7a14ebde8166426345ac2e
MineBill <minebill@protonmail.com>
2024-08-10 19:12:04 +0300

    Formatting.

--- Fussion/Source/Fussion/RHI/ShaderCompiler.h
+++ Fussion/Source/Fussion/RHI/ShaderCompiler.h
@@ -3,20 +3,18 @@
 #include <Fussion/Core/Maybe.h>
 
 namespace Fussion::RHI {
-class ShaderCompiler {
-public:
-    enum class CompileError {
+    class ShaderCompiler {
+    public:
+        enum class CompileError {};
 
+        struct CompileResult {
+            std::vector<ShaderStage> ShaderStages{};
+            ShaderMetadata Metadata{};
+        };
+
+        // TODO: Return some kind of error? The shaderc error api seems kind weird.
+        /// Tries to compile a shader file source code.
+        /// @returns The compilation result or nothing if the compilation failed.
+        static auto Compile(std::string const& source_code, std::string const& file_name = "(memory buffer)") -> Maybe<CompileResult>;
     };
-
-    struct CompileResult {
-        std::vector<ShaderStage> ShaderStages{};
-        ShaderMetadata Metadata{};
-    };
-
-    // TODO: Return some kind of error? The shaderc error api seems kind weird.
-    /// Tries to compile a shader file source code.
-    /// @returns The compilation result or nothing if the compilation failed.
-    static auto Compile(std::string const& source_code, std::string const& file_name = "(memory buffer)") -> Maybe<CompileResult>;
-};
 }
