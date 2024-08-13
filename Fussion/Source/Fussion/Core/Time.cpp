1a408312e9750950da428c815308d10e22badf53
MineBill <minebill@protonmail.com>
2024-08-13 23:08:49 +0300

    Formatting.

--- Fussion/Source/Fussion/Core/Time.cpp
+++ Fussion/Source/Fussion/Core/Time.cpp
@@ -2,15 +2,15 @@
 #include "Time.h"
 
 namespace Fussion {
-f32 Time::m_DeltaTime{ 0.0f };
+    f32 Time::m_DeltaTime{ 0.0f };
 
-f32 Time::DeltaTime()
-{
-    return m_DeltaTime;
-}
+    f32 Time::DeltaTime()
+    {
+        return m_DeltaTime;
+    }
 
-void Time::SetDeltaTime(f32 delta_time)
-{
-    m_DeltaTime = delta_time;
-}
+    void Time::SetDeltaTime(f32 delta_time)
+    {
+        m_DeltaTime = delta_time;
+    }
 }
--- Fussion/Source/Fussion/Core/Time.h
+++ Fussion/Source/Fussion/Core/Time.h
@@ -2,17 +2,16 @@
 #include "Fussion/Core/Types.h"
 
 namespace Fussion {
-class Application;
+    class Application;
 
-class Time {
-    friend Application;
+    class Time {
+        friend Application;
 
-public:
-    static f32 DeltaTime();
+    public:
+        static f32 DeltaTime();
 
-private:
-    static void SetDeltaTime(f32 delta_time);
-    static f32 m_DeltaTime;
-};
-
+    private:
+        static void SetDeltaTime(f32 delta_time);
+        static f32 m_DeltaTime;
+    };
 }
