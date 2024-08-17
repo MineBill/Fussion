df7f910e02361aca02e2603fcb37aade6c33cf84
MineBill <minebill@protonmail.com>
2024-08-18 00:03:22 +0300

    ThreadProtected wrapper that wrapps a mutex and an object and provides locked access to the object.

---
+++ Fussion/Source/Fussion/Core/ThreadProtected.h
@@ -0,0 +1,45 @@
+﻿#pragma once
+#include <mutex>
+
+namespace Fussion {
+    template<typename T>
+    class ThreadProtected {
+    public:
+        ThreadProtected() = default;
+
+        ThreadProtected(T const& value): m_Object(value) {}
+        ThreadProtected(T&& value): m_Object(std::move(value)) {}
+
+        template<typename Func>
+        auto Access(Func&& func) const
+        {
+            using ResultType = std::invoke_result_t<Func, T const&>;
+            std::lock_guard lock(m_Mutex);
+
+            if constexpr (std::is_void_v<ResultType>) {
+                func(m_Object);
+            } else {
+                return func(m_Object);
+            }
+        }
+
+        template<typename Func>
+        auto Access(Func&& func)
+        {
+            using ResultType = std::invoke_result_t<Func, T&>;
+            std::lock_guard lock(m_Mutex);
+
+            if constexpr (std::is_void_v<ResultType>) {
+                func(m_Object);
+            } else {
+                return func(m_Object);
+            }
+        }
+
+        T* UnsafePtr() { return &m_Object; }
+
+    private:
+        T m_Object;
+        mutable std::mutex m_Mutex;
+    };
+}
