1e993374686fdb77c2aed1efff9789996ab0a911
minebill <minebill@PC192.168.1.4>
2024-10-15 16:11:25 +0300

    Implement some linux system functions.

--- Fussion/Platform/Linux/LinuxSystem.cpp
+++ Fussion/Platform/Linux/LinuxSystem.cpp
@@ -1,6 +1,8 @@
 #include "Log/Log.h"
 #include "OS/System.h"
 
+#include <cstdlib>
+
 namespace Fussion::System {
     bool PrefersDark()
     {
@@ -14,8 +16,43 @@ namespace Fussion::System {
 
     auto GetKnownFolder(KnownFolders folder) -> std::filesystem::path
     {
-        (void)folder;
-        return {};
+        std::filesystem::path home = std::getenv("HOME");
+        if (home.empty()) {
+            LOG_WARNF("$HOME is not set");
+        }
+
+        std::filesystem::path path = "";
+        switch (folder) {
+        case KnownFolders::Downloads:
+            path = home / "Downloads";
+            break;
+        case KnownFolders::Config:
+            if (auto p = std::getenv("XDG_CONFIG_HOME")) {
+                path = p;
+            } else {
+                path = home / ".config";
+            }
+            break;
+        case KnownFolders::State:
+            if (auto p = std::getenv("XDG_STATE_HOME")) {
+                path = p;
+            } else {
+                path = home / ".local/state";
+            }
+            break;
+        case KnownFolders::Data:
+            if (auto p = std::getenv("XDG_DATA_HOME")) {
+                path = p;
+            } else {
+                path = home / ".local/share";
+            }
+            break;
+        case KnownFolders::Temp:
+            // name = "XDG_" break;
+            path = "/tmp";
+            break;
+        }
+        return path;
     }
 
     bool ConsoleSupportsColor()
