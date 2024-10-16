3dcf4b5b36f4f6a65fa502742b9c3bf7f2910868
minebill <minebill@protonmail.com>
2024-10-16 18:03:15 +0300

    Formatting.

--- Fussion/Source/Fussion/Core/BitFlags.h
+++ Fussion/Source/Fussion/Core/BitFlags.h
@@ -1,25 +1,25 @@
-﻿#pragma once
-
-
+#pragma once
 
 #define DECLARE_FLAGS(Enum, Flags)                                                       \
     struct Flags {                                                                       \
         using EnumType = Enum;                                                           \
         inline constexpr Flags() = default;                                              \
-        inline constexpr Flags(Enum v) : value(static_cast<int>(v)) {}                   \
+        inline constexpr Flags(Enum v)                                                   \
+            : value(static_cast<int>(v))                                                 \
+        { }                                                                              \
         inline constexpr bool test(Enum e) const { return value & static_cast<int>(e); } \
         inline constexpr operator int() const { return value; }                          \
-        inline constexpr void set(Enum e) {value |= static_cast<int>(e); }               \
-        inline constexpr void unset(Enum e) {value &= ~static_cast<int>(e); }            \
-        inline constexpr void toggle(Enum e) {                                           \
+        inline constexpr void set(Enum e) { value |= static_cast<int>(e); }              \
+        inline constexpr void unset(Enum e) { value &= ~static_cast<int>(e); }           \
+        inline constexpr void toggle(Enum e)                                             \
+        {                                                                                \
             if (test(e)) {                                                               \
                 unset(e);                                                                \
-            }                                                                            \
-            else {                                                                       \
+            } else {                                                                     \
                 set(e);                                                                  \
             }                                                                            \
         }                                                                                \
-        int value{};                                                                     \
+        int value {};                                                                    \
     };
 
 #define DECLARE_OPERATORS_FOR_FLAGS(Flags)                                                                           \
@@ -29,6 +29,6 @@
     inline Flags operator|(Flags a, Flags b) { return Flags(Flags::EnumType(a.value | b.value)); }                   \
     inline Flags operator|=(Flags& a, Flags::EnumType b) { return a = Flags(Flags::EnumType(a.value | int(b))); }
 
-#define BITFLAGS(Enum) \
-    DECLARE_FLAGS(Enum, Enum ## Flags) \
-    DECLARE_OPERATORS_FOR_FLAGS(Enum ## Flags)
+#define BITFLAGS(Enum)               \
+    DECLARE_FLAGS(Enum, Enum##Flags) \
+    DECLARE_OPERATORS_FOR_FLAGS(Enum##Flags)
